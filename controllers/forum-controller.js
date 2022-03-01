4444a63f0f21ec16d97e14fba9e38b51dfd600c6
Humzah Wasim <humzah99@live.co.uk>
2022-03-01 15:28:22 +0000

    Finishing forum backend to mongoose connection and adding user sign up functionality

--- controllers/forum-controller.js
+++ controllers/forum-controller.js
@@ -1,13 +1,16 @@
-const { v4: uuidv4 } = require("uuid");
-const { validationResult } = require("express-validator");
+const {
+    v4: uuidv4
+} = require("uuid");
+const {
+    validationResult
+} = require("express-validator");
 const HttpError = require("../models/http-error");
 const Forum = require("../models/forum");
-let DUMMY_QUESTIONS = [
-  {
-    heading: "How can I construct a HTML table?",
-    text: "Please can I have some information on the basics of constructing a HTML table? Here is my code...",
-    image: null,
-    codeString: `<table>
+let DUMMY_QUESTIONS = [{
+        heading: "How can I construct a HTML table?",
+        text: "Please can I have some information on the basics of constructing a HTML table? Here is my code...",
+        image: null,
+        codeString: `<table>
       <form>
         <div class="mb-3">
           <label for="exampleInputEmail1" class="form-label">Email address</label
@@ -35,8 +38,8 @@ let DUMMY_QUESTIONS = [
         <button type="submit" class="btn btn-primary">Submit</button>
       </form>
     </table>`,
-    codeResponses: [
-      `<table>
+        codeResponses: [
+            `<table>
       <tr>
         <th>User Form</th>
       </tr>
@@ -76,139 +79,199 @@ let DUMMY_QUESTIONS = [
         </td>
       </tr>
     </table>`,
-    ],
-    answers: [
-      "See the following link for more information on HTML tables. https://www.w3schools.com/html/html_tables.asp",
-      "Navigate to this website's HTML table documentation for more help.",
-    ],
-    user: '1'
-  },
-  {
-    heading: "My HTML heading does not render on screen.",
-    text: "Can anyone help me identify the issue with this code that is not allowing any headings to render on to the screen?",
-    image: null,
-    codeString: "<h7>Hello world!</h7>",
-    codeResponses: [],
-    answers: [
-      "You cannot render a <h7> tag. Read the HTML headings documentation on this website for more information.",
-      "Have a look at the following link for HTML headings... https://www.w3schools.com/html/html_headings.asp ",
-    ],
-    user: '1'
-  },
+        ],
+        answers: [
+            "See the following link for more information on HTML tables. https://www.w3schools.com/html/html_tables.asp",
+            "Navigate to this website's HTML table documentation for more help.",
+        ],
+        user: '1'
+    },
+    {
+        heading: "My HTML heading does not render on screen.",
+        text: "Can anyone help me identify the issue with this code that is not allowing any headings to render on to the screen?",
+        image: null,
+        codeString: "<h7>Hello world!</h7>",
+        codeResponses: [],
+        answers: [
+            "You cannot render a <h7> tag. Read the HTML headings documentation on this website for more information.",
+            "Have a look at the following link for HTML headings... https://www.w3schools.com/html/html_headings.asp ",
+        ],
+        user: '1'
+    },
 ];
 
 const getAllForums = (req, res, next) => {
-  if (!DUMMY_QUESTIONS) {
-    throw new HttpError("Could not locate the user forum", 404);
-  }
-  res.json({ DUMMY_QUESTIONS });
+    if (!DUMMY_QUESTIONS) {
+        throw new HttpError("Could not locate the user forum", 404);
+    }
+    res.json({
+        DUMMY_QUESTIONS
+    });
 };
 
-const getForumQuestionById = async (req, res, next) => {
-  const questionId = req.params.questionId;
+const getForumQuestionById = async(req, res, next) => {
+    const questionId = req.params.questionId;
 
-  let forumQuestion;
-  try {
-    forumQuestion = await Forum.findById(questionId);
-  } catch (err) {
-    const error = new HttpError(
-      'Something went wrong, could not find a question.', 500
-    );
-    return next(error);
-  }
+    let forumQuestion;
+    try {
+        forumQuestion = await Forum.findById(questionId);
+    } catch (err) {
+        const error = new HttpError(
+            'Something went wrong, could not find a question.', 500
+        );
+        return next(error);
+    }
 
-  if (!forumQuestion) {
-    const error = new HttpError("Could not find a question for the provided id",
-      404)
-    return next(error);
-  }
-  res.json({ forumQuestion: forumQuestion.toObject({ getters: true }) });
+    if (!forumQuestion) {
+        const error = new HttpError("Could not find a question for the provided id",
+            404)
+        return next(error);
+    }
+    res.json({
+        forumQuestion: forumQuestion.toObject({
+            getters: true
+        })
+    });
 };
 
-const getForumQuestionByUserId = async (req, res, next) => {
-  const userId = req.params.userId;
+const getForumQuestionByUserId = async(req, res, next) => {
+    const userId = req.params.userId;
 
-  let forumQuestions;
-  try {
-    forumQuestions = await Forum.find({ user: userId });
-  } catch (err) {
-    const error = new HttpError(
-      'Fetching questions failed, please try again later',
-      500
-    );
-    return next(error);
-  }
+    let forumQuestions;
+    try {
+        forumQuestions = await Forum.find({
+            user: userId
+        });
+    } catch (err) {
+        const error = new HttpError(
+            'Fetching questions failed, please try again later',
+            500
+        );
+        return next(error);
+    }
 
-  if (!forumQuestions || forumQuestions.length === 0) {
-    const error = new HttpError("Could not find a question for the provided id",
-      404)
-    return next(error);
-  }
-  res.json({ forumQuestions: forumQuestions.map(forumQuestion => forumQuestion.toObject({ getters: true })) });
+    if (!forumQuestions || forumQuestions.length === 0) {
+        const error = new HttpError("Could not find a question for the provided id",
+            404)
+        return next(error);
+    }
+    res.json({
+        forumQuestions: forumQuestions.map(forumQuestion => forumQuestion.toObject({
+            getters: true
+        }))
+    });
 };
 
-const addQuestion = async (req, res, next) => {
-  const errors = validationResult(req);
+const addQuestion = async(req, res, next) => {
+    const errors = validationResult(req);
 
-  if (!errors.isEmpty()) {
-    console.log(errors);
-    throw new HttpError("Invalid inputs passed, please check your data.", 422);
-  }
-  const { heading, text, image, codeString, codeResponses, answers, user } = req.body;
-  const addedQuestion = new Forum({
-    heading,
-    text,
-    image,
-    codeString,
-    codeResponses,
-    answers,
-    user
-  });
+    if (!errors.isEmpty()) {
+        console.log(errors);
+        throw new HttpError("Invalid inputs passed, please check your data.", 422);
+    }
+    const {
+        heading,
+        text,
+        image,
+        codeString,
+        codeResponses,
+        answers,
+        user
+    } = req.body;
+    const addedQuestion = new Forum({
+        heading,
+        text,
+        image,
+        codeString,
+        codeResponses,
+        answers,
+        user
+    });
 
-  try {
-    await addedQuestion.save();
-  } catch (err) {
-    const error = new HttpError(
-      'Creating question failed, please try again',
-      500
-    );
-    return next(error);
-  }
+    try {
+        await addedQuestion.save();
+    } catch (err) {
+        const error = new HttpError(
+            'Creating question failed, please try again',
+            500
+        );
+        return next(error);
+    }
 
 
-  res.status(201).json({ question: addedQuestion });
+    res.status(201).json({
+        question: addedQuestion
+    });
 };
 
-const updateQuestion = (req, res, next) => {
-  const errors = validationResult(req);
+const updateQuestion = async(req, res, next) => {
+    const errors = validationResult(req);
 
-  if (!errors.isEmpty()) {
-    console.log(errors);
-    throw new HttpError("Invalid inputs passed, please check your data.", 422);
-  }
-  const { heading, text, image, codeString } = req.body;
-  const questionId = req.params.questionId;
+    if (!errors.isEmpty()) {
+        console.log(errors);
+        return next(new HttpError("Invalid inputs passed, please check your data.", 422));
+    }
+    const {
+        heading,
+        text,
+        image,
+        codeString
+    } = req.body;
+    const questionId = req.params.questionId;
 
-  const updatedQuestion = {
-    ...DUMMY_QUESTIONS.find((fq) => fq.id === questionId),
-  };
-  const questionIndex = DUMMY_QUESTIONS.findIndex((fq) => fq.id === questionId);
-  updatedQuestion.heading = heading;
-  updatedQuestion.text = text;
-  updatedQuestion.image = image;
-  updatedQuestion.codeString = codeString;
+    let forumQuestion;
+    try {
+        forumQuestion = await Forum.findById(questionId);
+    } catch (err) {
+        const error = new HttpError(
+            'Something went wrong, could not update question.', 500
+        );
+        return next(error);
+    }
+    forumQuestion.heading = heading;
+    forumQuestion.text = text;
+    forumQuestion.image = image;
+    forumQuestion.codeString = codeString;
 
-  DUMMY_QUESTIONS[questionIndex] = updatedQuestion;
+    try {
+        await forumQuestion.save();
+    } catch (err) {
+        const error = new HttpError(
+            'Something went wrong, could not update question.', 500
+        );
+        return next(error);
+    }
 
-  res.status(200).json({ question: updatedQuestion });
+    res.status(200).json({
+        question: forumQuestion.toObject({
+            getters: true
+        })
+    });
 };
-const deleteQuestion = (req, res, next) => {
-  const questionId = req.params.questionId;
-  if (!DUMMY_QUESTIONS.find(q => q.id === questionId)) {
-    throw new HttpError('Could not find a question for that id.', 404);
-  }
-  DUMMY_QUESTIONS = DUMMY_QUESTIONS.filter((fq) => fq.id !== questionId);
-  res.status(200).json({ message: "Deleted question." });
+const deleteQuestion = async(req, res, next) => {
+    const questionId = req.params.questionId;
+
+    let forumQuestion;
+    try {
+        forumQuestion = await Forum.findById(questionId);
+    } catch (err) {
+        const error = new HttpError(
+            'Something went wrong, could not delete question.', 500
+        );
+        return next(error);
+    }
+
+    try {
+        await forumQuestion.remove();
+    } catch (err) {
+        const error = new HttpError(
+            'Something went wrong, could not delete question.', 500
+        );
+        return next(error);
+    }
+    res.status(200).json({
+        message: "Deleted question."
+    });
 };
 
 exports.getAllForums = getAllForums;
--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,88 +1,132 @@
-const { validationResult } = require("express-validator");
+const {
+    validationResult
+} = require("express-validator");
 const HttpError = require("../models/http-error");
-
-const DUMMY_USERS = [
-  {
+const User = require("../models/user");
+const DUMMY_USERS = [{
     username: "Humzah99",
     name: "Humzah Wasim",
     email: "humzah99@live.co.uk",
     password: "Humzah#999",
-  },
-];
+}, ];
 
 const getUserByUsername = (req, res, next) => {
-  const username = req.params.username;
-  const user = DUMMY_USERS.find((u) => {
-    return u.username === username;
-  });
-  if (!user) {
-    return next(
-      new HttpError("Could not find a user for the provided username"),
-      404
-    );
-  }
-  res.json({ user });
+    const username = req.params.username;
+    const user = DUMMY_USERS.find((u) => {
+        return u.username === username;
+    });
+    if (!user) {
+        return next(
+            new HttpError("Could not find a user for the provided username"),
+            404
+        );
+    }
+    res.json({
+        user
+    });
 };
 
-const signup = (req, res, next) => {
-  const errors = validationResult(req);
+const signup = async(req, res, next) => {
+    const errors = validationResult(req);
 
-  if (!errors.isEmpty()) {
-    console.log(errors);
-    throw new HttpError("Invalid inputs passed, please check your data.", 422);
-  }
-  const { username, name, email, password } = req.body;
+    if (!errors.isEmpty()) {
+        return next(new HttpError("Invalid inputs passed, please check your data.", 422));
+    }
+    const {
+        username,
+        name,
+        email,
+        password,
+        questions
+    } = req.body;
 
-  const duplicateEmail = DUMMY_USERS.find((u) => u.email === email);
-  const duplicateUsername = DUMMY_USERS.find((u) => u.username === username);
+    let existingUser
+    try {
+        existingUser = await User.findOne({
+            email: email
+        })
+    } catch (err) {
+        const error = new HttpError(
+            'Signing up failed, please try again later.',
+            500
+        )
+        return next(error);
+    }
 
-  if (duplicateEmail || duplicateUsername) {
-    throw new HttpError(
-      "Could not create user, email or username already exists",
-      422
-    );
-  }
-  const createdUser = {
-    username,
-    name,
-    email,
-    password,
-  };
+    if (existingUser) {
+        const error = new HttpError(
+            'User exists already, please login instead.',
+            422
+        );
+        return next(error);
+    }
+    const createdUser = new User({
+        username,
+        name,
+        email,
+        password,
+        image: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+        questions
+    });
+    try {
+        await createdUser.save();
+    } catch (err) {
+        const error = new HttpError(
+            'Signing up failed, please try again',
+            500
+        );
+        return next(error);
+    }
 
-  DUMMY_USERS.push(createdUser);
 
-  res.status(201).json({ user: createdUser });
+
+    res.status(201).json({
+        user: createdUser.toObject({
+            getters: true
+        })
+    });
 };
 
 const login = (req, res, next) => {
-  const { email, password } = req.body;
+    const {
+        email,
+        password
+    } = req.body;
 
-  const identifiedUser = DUMMY_USERS.find((u) => u.email === email);
-  if (!identifiedUser || identifiedUser.password !== password) {
-    throw new HttpError(
-      "Could not identify user, credentials seem to be wrong",
-      401
-    );
-  }
+    const identifiedUser = DUMMY_USERS.find((u) => u.email === email);
+    if (!identifiedUser || identifiedUser.password !== password) {
+        throw new HttpError(
+            "Could not identify user, credentials seem to be wrong",
+            401
+        );
+    }
 
-  res.json({ message: "Logged in" });
+    res.json({
+        message: "Logged in"
+    });
 };
 
 const updateUser = (req, res, next) => {
-  const { name, email, password } = req.body;
-  const userName = req.params.username;
+    const {
+        name,
+        email,
+        password
+    } = req.body;
+    const userName = req.params.username;
 
-  const updatedUser = {
-    ...DUMMY_USERS.find((u) => u.username === userName),
-  };
-  const userIndex = DUMMY_USERS.findIndex((u) => u.username === userName);
-  updatedUser.name = name;
-  updatedUser.email = email;
-  updatedUser.password = password;
+    const updatedUser = {
+        ...DUMMY_USERS.find((u) => u.username === userName),
+    };
+    const userIndex = DUMMY_USERS.findIndex((u) => u.username === userName);
+    updatedUser.name = name;
+    updatedUser.email = email;
+    updatedUser.password = password;
 
-  DUMMY_USERS[userIndex] = updatedUser;
+    DUMMY_USERS[userIndex] = updatedUser;
 
-  res.status(200).json({ user: updatedUser });
+    res.status(200).json({
+        user: updatedUser
+    });
 };
 
 exports.getUserByUsername = getUserByUsername;
