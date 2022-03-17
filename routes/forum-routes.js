f084fc131fa84b1d9e7d91fcebbab7b9147b4d4b
Humzah99 <humzah99@live.co.uk>
2022-03-17 19:28:23 +0000

    user authorization on the backend

--- routes/forum-routes.js
+++ routes/forum-routes.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const { check } = require("express-validator");
-
+const checkAuth = require("../middleware/check-auth")
 const forumController = require("../controllers/forum-controller");
 
 const router = express.Router();
 
+
+
 router.get("/", forumController.getAllForums);
 router.get("/:questionId", forumController.getForumQuestionById);
+
+router.use(checkAuth);
+
 router.get("/user/:userId", forumController.getForumQuestionByUserId);
 router.post(
   "/",
